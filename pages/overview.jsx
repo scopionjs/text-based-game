57b06a998be16b61f1637327df56d1870112dabf
scopion js <josephsiame233@gmail.com>
2022-07-27 06:58:50 +0200

    push4

--- pages/overview.jsx
+++ pages/overview.jsx
@@ -8,11 +8,7 @@ import Head from "next/head"
 export default function OverView() {
     return(
         <>
-        <Head>
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-<link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet" />
-        </Head>
+        
         <Nav />
         <div className="over-view-bars-wrapper" >
             <LeftBar />
