ac01a335084e1d48711b8b5acab7f4cf8e730675
scopion js <josephsiame233@gmail.com>
2022-07-27 07:04:42 +0200

    push5

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
     
       <Head>
             <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='true' />
 <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet" />
         </Head>
       <body>
